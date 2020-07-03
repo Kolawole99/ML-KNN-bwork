04605d7c99ad1431a5921c181aad7738e09ae6ec
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-03 14:12:02 +0100

    Importing the needed packages

---
+++ KNN.py
@@ -0,0 +1,13 @@
+#====================================DATA FORMATTING, PROJECT=================================
+
+#=========================importing needed packages and source files=========================
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+#%matplotlib inline #needed in Jupyter labs
+
