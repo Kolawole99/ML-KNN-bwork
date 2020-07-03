38d8b94c0cb92393a125756aaa7494cc9f96edc9
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-03 15:00:11 +0100

    Data visualization and plotting using principal and age columns as the data sample

--- KNN.py
+++ KNN.py
@@ -9,9 +9,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.ticker as ticker
 from sklearn import preprocessing
+import seaborn as sns
 #%matplotlib inline #needed in Jupyter labs
 
-#======================================About the dataset=====================================
+#====================================About the dataset=====================================
 # This dataset is about past loans. The Loan_train.csv data set includes details of 346 customers whose loan are already paid off or defaulted. It includes following fields:
 
 # Loan_status	        Whether a loan is paid off on in collection
@@ -30,9 +31,35 @@ datashape = df.shape
 print(datashow)
 print(datashape)
 
-#===========================Converting all dates to date time object================================
+#======================Converting all dates to date time object================================
 df['due_date'] = pd.to_datetime(df['due_date'])
 df['effective_date'] = pd.to_datetime(df['effective_date'])
 df.head()
 
-#=======================
+
+#=================================DATA VISUALIZATION AND PREPROCESSING====================================
+
+#=========================================Class value count======================================
+valuecounts = df['loan_status'].value_counts()
+print(valuecounts)
+
+#==============================Plot histogram to understand this data better===========================
+##########============Using Principal================#########
+#setting the space, inital and final value then the samples to generate
+bins = np.linspace(df.Principal.min(), df.Principal.max(), 10)
+#setting the grids to plot and their titles, colors and how they wrap
+g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
+#setting subsets
+g.map(plt.hist, 'Principal', bins=bins, ec="k")
+#auto detection of element to be displayed in the axis.
+g.axes[-1].legend()
+plt.show()
+
+#############================Using Age==================##############
+bins = np.linspace(df.age.min(), df.age.max(), 10)
+g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
+g.map(plt.hist, 'age', bins=bins, ec="k")
+g.axes[-1].legend()
+plt.show()
+
+
