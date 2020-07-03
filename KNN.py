80622bf61ec55ca4990ffeebec1ffa57038baf4f
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-03 14:12:41 +0100

    About the data and loading the data into a dataframe and saving the dataset.

--- KNN.py
+++ KNN.py
@@ -11,3 +11,19 @@ import matplotlib.ticker as ticker
 from sklearn import preprocessing
 #%matplotlib inline #needed in Jupyter labs
 
+#======================================About the dataset=====================================
+# This dataset is about past loans. The Loan_train.csv data set includes details of 346 customers whose loan are already paid off or defaulted. It includes following fields:
+
+# Loan_status	        Whether a loan is paid off on in collection
+# Principal	            Basic principal loan amount at the
+# Terms	                Origination terms which can be weekly (7 days), biweekly, and monthly payoff schedule
+# Effective_date	    When the loan got originated and took effects
+# Due_date	            Since it’s one-time payoff schedule, each loan has one single due date
+# Age	                Age of applicant
+# Education	            Education of applicant
+# Gender	            The gender of applicant
+
+
+#==========================loading the data into a dataframe==============================
+df = pd.read_csv('./loan_train.csv')
+df.head()
